d0638fcfea95c1c2b28ae14185d73af15455e4b2
Syrhundred <91780657+Syrhundred@users.noreply.github.com>
2022-12-04 18:37:53 +0600

    poterial commenting zhartysyn

--- src/com/company/Posts.java
+++ src/com/company/Posts.java
@@ -5,12 +5,33 @@ import java.util.HashMap;
 
 public class Posts {
     String posts;
-    HashMap<Posts, String> comments;
+    ArrayList<String> comments;
     int likes;
 
-    public Posts(String posts, int likes, HashMap<Posts, String> comments){
+    public Posts(String posts, int likes, ArrayList<String> comments){
         this.posts = posts;
         this.likes = likes;
-        this.comments = comments;
+        this.comments = new ArrayList<>();
+    }
+
+    public void addComment(String comment){
+        comments.add(comment);
+    }
+
+//    public String toString(){
+//        StringBuilder builder = new StringBuilder();
+//
+//        for(String comment : comments) {
+//            builder.append(comment);
+//        }
+//        return builder + "";
+//    }
+
+    public String getComments(){
+        StringBuilder commentsB = new StringBuilder();
+        for (int i = 0; i < comments.size(); i++) {
+            commentsB.append(comments.get(i));
+        }
+        return comments.toString();
     }
 }
